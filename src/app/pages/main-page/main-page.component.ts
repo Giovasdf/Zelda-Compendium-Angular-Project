9074fd2ffd2781697e34e5ccce8795fc080246b0
Giovanni Molina <giovanni.m.05@gmail.com>
2023-10-08 23:11:45 +0100

    ✨feature: Filter real time

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,5 +1,5 @@
+import { Monster } from './../../interface/monster';
 import { Component, OnInit } from '@angular/core';
-import { Monster } from 'src/app/interface/monster';
 import { CompendiumService } from 'src/app/services/compendium.service';
 
 @Component({
@@ -9,28 +9,34 @@ import { CompendiumService } from 'src/app/services/compendium.service';
 })
 export class MainPageComponent {
 
-  monsters: Monster[] | undefined;
-  monsterCopy: Monster[] | undefined;
+  monsters!: Monster[];
+  monsterCopy!: Monster[];
   constructor(public serviceCompendium:CompendiumService) {
     this.getData();
-
+    this.monsterCopy = [];
    }
 
 
-  filter(e:any){
-    const search:string = e.target.value;
+
+   async getData() {
+    (await this.serviceCompendium.getAll()).subscribe((data: any) => {
+
+      this.monsters = data.data.map(({ name, id, category, description, image, common_locations, drops, dlc }: Monster) => {
+        return { name, id, category, description, image, common_locations, drops, dlc };
+      });
+
+      this.monsterCopy = [...this.monsters]; // Clonar el arreglo original
+      console.log("Monsters:", this.monsters);
+      console.log("MonsterCopy:", this.monsterCopy);
+    });
   }
 
-  async getData(){
-    (await this.serviceCompendium.getAll()).subscribe((data:any)=>{
-      console.table(data.data);
-
-      this.monsters = data.data.map(({name,id,category,description,image,common_locations,drops,dlc}:any)=>{
-        return {name,id,category,description,image,common_locations,drops,dlc};
-      })
-
-      this.monsterCopy = this.monsters;
-    })
+  filter(e: any) {
+    const search: string = e.target.value;
+    this.monsters = this.monsterCopy?.filter(({ name }: Monster) => {
+      return name.toLowerCase().includes(search.toLowerCase());
+    });
   }
+
 
 }
